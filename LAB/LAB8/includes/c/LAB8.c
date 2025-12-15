afc78177246cc934f5e56f951a0141e02bf72ebe
Roko <rokovukovic191@gmail.com>
2025-12-15 18:21:14 +0100

    LAB8 done

--- LAB/LAB8/includes/c/LAB8.c
+++ LAB/LAB8/includes/c/LAB8.c
@@ -19,7 +19,7 @@ FILE* getFile(const char* path){
     return file;
 }
 
-int createNode(Node** p_node, int el){
+int createNode(Node** p_node, float el){
     if(!p_node){
         return -1;
     }
@@ -70,7 +70,7 @@ int printList(Node* p_head){
     return 0;
 }
 
-int getLastEl(Node* p_head){
+float getLastEl(Node* p_head){
     if(!p_head){
         return -1;
     }
@@ -95,6 +95,22 @@ int pop(Node* p_head){
     return 0;
 }
 
+int isNumCheck(const char* num){
+    if(!num){
+        return 1;
+    }
+
+    int cnt = 0;
+    int status = 0;
+    while(num[cnt] != '\0') {
+        if(!isdigit(num[cnt])) status = 2;
+        cnt++;
+    }
+
+
+    return  status;
+}
+
 float postfix(const char* pf_e, size_t size){
     if(!pf_e || size == 0){
         return -1;
@@ -107,66 +123,48 @@ float postfix(const char* pf_e, size_t size){
     }
     stack -> next = NULL;
 
+    char* cpy_pf_e = malloc(size + 1);
+    if(!cpy_pf_e) return -2;
 
-    for(int i=0;  i < size; i++){
-        // U slucaju da je broj dodajemo na stack
-        if(isdigit(pf_e[i])){
-                char* end;
-                float val = strtof(&pf_e[i], &end);
-                printf("[INFO] postfix val thats gonna be pushed: %.2f\n", val);
-                push(stack, val);
-                printList(stack);
-                i += (end - &pf_e[i]) - 1;
-                continue;
+    memcpy(cpy_pf_e, pf_e, size);
+    cpy_pf_e[size] = '\0';
+    
+    char *saveptr;
+    char *word = strtok_r(cpy_pf_e, " \n", &saveptr);
+    
+    while(word){
+        if(!strcmp("+", word)){
+            float second = getLastEl(stack);
+            pop(stack);
+            float first = getLastEl(stack);
+            pop(stack);
+            push(stack,first + second);
+        } else if(!strcmp("-", word)) {
+            float second = getLastEl(stack);
+            pop(stack);
+            float first = getLastEl(stack);
+            pop(stack);
+            push(stack, first - second);
+        } else if (!strcmp("*", word)){
+            float second = getLastEl(stack);
+            pop(stack);
+            float first = getLastEl(stack);
+            pop(stack);
+            push(stack, first * second);
+        } else if(!strcmp("/", word)){
+            float second = getLastEl(stack);
+            pop(stack);
+            float first = getLastEl(stack);
+            pop(stack);
+            push(stack, first / second);
         } else {
-            // Obavimo zadanu matematicku operaciju u slucaju da nije
-            // PREDPOSTAVKA JE DA IMAMO VALJAN POSTIFIX izraz
-            switch(pf_e[i]){
-                case '+': {
-                    float second = getLastEl(stack);
-                    pop(stack);
-                    float first = getLastEl(stack);
-                    pop(stack);
+            float val = atof(word);
+            push(stack, val);
+        }
 
-                    push(stack,(float)first + second);
-
-                    break;
-                }
-                case '-': {
-                    float second = getLastEl(stack);
-                    pop(stack);
-                    float first = getLastEl(stack);
-                    pop(stack);
-
-                    push(stack, (float)first - second);
-
-                    break;
-                }
-                case '*': {
-                    float second = getLastEl(stack);
-                    pop(stack);
-                    float first = getLastEl(stack);
-                    pop(stack);
-
-                    push(stack,first * second);
-
-                    break;
-                }
-
-                case '/': {
-                    float second = getLastEl(stack);
-                    pop(stack);
-                    float first = getLastEl(stack);
-                    pop(stack);
-
-                    push(stack, first / second);
-
-                    break;
-                }
-            }
-        } 
-        
+        word = strtok_r(NULL, " \n", &saveptr);
     }
+
     return stack -> next -> el;
 }
 
@@ -185,7 +183,16 @@ int readFileAndCalculate(FILE* file){
         if(line[0] == '#'){
             continue;
         }
-        printf("Rezultat postfix izraza %s: %.2f\n", line, postfix(line, sizeLine));
+
+        float res = postfix(line, sizeLine);
+        
+        size_t len = strlen(line);
+        if (len > 0 && line[len - 1] == '\n') {
+            line[len - 1] = '\0';
+            len--;                
+        }
+
+        printf("Rezultat postfix izraza %s: %.2f\n", line, res);
     }
     
     free(line);
